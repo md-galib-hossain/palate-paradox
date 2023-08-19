9b87e39f3305d60c6117b9d5ed03be9cc2487adc
md-galib-hossain <mdgalib23@gmail.com>
2023-08-19 14:15:52 +0600

    Parallax added

--- palate-paradox-client/src/Pages/Menu/Menu.jsx
+++ palate-paradox-client/src/Pages/Menu/Menu.jsx
@@ -2,13 +2,22 @@ import React from "react";
 import { Helmet } from "react-helmet-async";
 import Cover from "../Shared/Cover/Cover";
 import menuimg from "../../assets/menu/banner3.jpg"
+import PopularMenu from "../Home/PopularMenu/PopularMenu";
 const Menu = () => {
   return (
     <div className="">
       <Helmet>
         <title>Menu</title>
       </Helmet>
-    <Cover img={menuimg} height ={"600px"}></Cover>
+    <Cover img={menuimg} height ={"600px"} title="our menu"></Cover>
+    <PopularMenu></PopularMenu>
+    <Cover img={menuimg} height ={"600px"} title="our menu"></Cover>
+    <PopularMenu></PopularMenu>
+    <Cover img={menuimg} height ={"600px"} title="our menu"></Cover>
+    <PopularMenu></PopularMenu>
+    <Cover img={menuimg} height ={"600px"} title="our menu"></Cover>
+    <PopularMenu></PopularMenu>
+   
     </div>
   );
 };
--- palate-paradox-client/src/Pages/Shared/Cover/Cover.jsx
+++ palate-paradox-client/src/Pages/Shared/Cover/Cover.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
+import { Parallax } from 'react-parallax';
 
-const Cover = ({img,height}) => {
+const Cover = ({img,height,title}) => {
   return (
-    <div className={`hero h-[${height}]`} style={{backgroundImage: `url("${img}")`}}>
+<Parallax
+        blur={{ min: -50, max: 50 }}
+        bgImage={img}
+        bgImageAlt="Menu"
+        strength={-200}
+    >
+      <div className={`hero h-[${height}]`}>
   <div className="hero-overlay bg-opacity-60"></div>
   <div className="hero-content text-center text-neutral-content">
     <div className="max-w-md">
-      <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+      <h1 className="mb-5 text-5xl font-bold uppercase">{title}</h1>
       <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button className="btn btn-primary">Get Started</button>
     </div>
   </div>
 </div>
+    </Parallax>
+
+   
   )
 }
 
