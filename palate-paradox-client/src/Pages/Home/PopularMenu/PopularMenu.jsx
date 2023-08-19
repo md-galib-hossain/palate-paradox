25872d1acf08f723994269a8de449b82d4f11554
md-galib-hossain <mdgalib23@gmail.com>
2023-08-19 16:17:10 +0600

    Custom hook added

--- palate-paradox-client/src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ palate-paradox-client/src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,17 +1,11 @@
-import React, { useEffect, useState } from "react";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 import MenuItem from "../../Shared/MenuItem/MenuItem";
+import useMenu from "../../../hooks/useMenu";
 
 const PopularMenu = () => {
-  const [menu, setMenu] = useState([]);
-  useEffect(() => {
-    fetch("menu.json")
-      .then((res) => res.json())
-      .then((data) => {
-        const PopularMenu = data.filter((item) => item.category === "popular");
-        setMenu(PopularMenu);
-      });
-  }, []);
+ 
+  const [menu] = useMenu()
+  const popularMenu = menu.filter(item => item.category === "popular")
   return (
     <section className="max-w-screen-xl mx-auto mb-32">
       <SectionTitle
@@ -22,7 +16,7 @@ const PopularMenu = () => {
       <div className="grid md:grid-cols-2 gap-8">
 
         {
-            menu.map(item=> <MenuItem key={item._id} item={item}>
+            popularMenu.map(item=> <MenuItem key={item._id} item={item}>
             </MenuItem>)
         }
       </div>
