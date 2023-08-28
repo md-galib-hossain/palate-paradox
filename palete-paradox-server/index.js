02ad9ab3334e825b137a847b827429a69b08676e
md-galib-hossain <mdgalib23@gmail.com>
2023-08-28 10:40:59 +0600

    Database connected and usemenu hook data fetched from database

--- palete-paradox-server/index.js
+++ palete-paradox-server/index.js
@@ -1,16 +1,57 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
-const port = process.env.PORT
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT;
 // middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.send("heyyyy")
+// database configuration
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fkxltzv.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const menuCollection = client.db("paradoxdb").collection("menu");
+    const reviewCollection = client.db("paradoxdb").collection("reviews");
+
+app.get("/menu", async(req,res)=>{
+    const result = await menuCollection.find().toArray()
+    res.send(result)
 })
 
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`)
-})
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("heyyyy");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
