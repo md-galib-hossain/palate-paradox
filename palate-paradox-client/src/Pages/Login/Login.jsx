aaac5481f8193fa788a8bc3cceb1fc1c1af0146d
md-galib-hossain <mdgalib23@gmail.com>
2023-09-03 11:35:20 +0600

    Login Form added

--- palate-paradox-client/src/Pages/Login/Login.jsx
+++ palate-paradox-client/src/Pages/Login/Login.jsx
@@ -1,14 +1,20 @@
 
 const Login = () => {
+    const handleLogin = event=>{
+        event.preventDefault()
+        const form = event.target;
+        const email = form.email.value
+        const password = form.password.value
+    }
   return (
         <div className="hero min-h-screen bg-base-200">
   <div className="hero-content flex-col lg:flex-row-reverse">
-    <div className="text-center lg:text-left">
+    <div className="text-center w-1/2 lg:text-left lg:ps-10">
       <h1 className="text-5xl font-bold">Login now!</h1>
       <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
     </div>
-    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-      <form className="card-body">
+    <div className="card md:w-1/2 max-w-sm shadow-2xl bg-base-100">
+      <form className="card-body" onSubmit={handleLogin}>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
