b8b8530bd24543f246d2cb7c23dd6fcae9ec004e
md-galib-hossain <mdgalib23@gmail.com>
2023-09-03 10:43:51 +0600

    Reviews loaded

--- palete-paradox-server/index.js
+++ palete-paradox-server/index.js
@@ -34,6 +34,10 @@ app.get("/menu", async(req,res)=>{
     const result = await menuCollection.find().toArray()
     res.send(result)
 })
+app.get("/reviews", async(req,res)=>{
+    const result = await reviewCollection.find().toArray()
+    res.send(result)
+})
 
 
     // Send a ping to confirm a successful connection
