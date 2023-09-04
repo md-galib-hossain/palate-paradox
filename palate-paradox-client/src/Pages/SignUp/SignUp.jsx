b7ad482be8b3d3bb21331627b9e376fec3db03f6
md-galib-hossain <mdgalib23@gmail.com>
2023-09-04 11:36:07 +0600

    React hook form validation

--- palate-paradox-client/src/Pages/SignUp/SignUp.jsx
+++ palate-paradox-client/src/Pages/SignUp/SignUp.jsx
@@ -1,67 +1,98 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 const SignUp = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="hero min-h-screen bg-base-200">
-    <div className="hero-content flex-col lg:flex-row-reverse">
-      <div className="text-center w-1/2 lg:text-left lg:ps-10">
-        <h1 className="text-5xl font-bold">Sign Up Now</h1>
-        <p className="py-6">
-          Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-          excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-          a id nisi.
-        </p>
-      </div>
-      <div className="card md:w-1/2 max-w-sm shadow-2xl bg-base-100">
-        <form className="card-body">
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Name</span>
-            </label>
-            <input
-              type="text"
-              name="name"
-              placeholder="name"
-              className="input input-bordered"
-            />
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Email</span>
-            </label>
-            <input
-              type="email"
-              name="email"
-              placeholder="email"
-              className="input input-bordered"
-            />
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Password</span>
-            </label>
-            <input
-              type="password"
-              name="password"
-              placeholder="password"
-              className="input input-bordered"
-            />
-          </div>
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center w-1/2 lg:text-left lg:ps-10">
+          <h1 className="text-5xl font-bold">Sign Up Now</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+        </div>
+        <div className="card md:w-1/2 max-w-sm shadow-2xl bg-base-100">
+          <form className="card-body" onSubmit={handleSubmit(onSubmit)}>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                name="name"
+                placeholder="name"
+                {...register("name", { required: true })}
+                className="input input-bordered"
+              />
+              {errors.name && (
+                <span className="text-red-500">This field is required</span>
+              )}
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                name="email"
+                {...register("email", { required: true })}
+                placeholder="email"
+                className="input input-bordered"
+              />
+              {errors.email && (
+                <span className="text-red-500">This field is required</span>
+              )}
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                name="password"
+                {...register("password", { required: true , minLength: 6,maxLength: 20 })}
+                placeholder="password"
+                className="input input-bordered"
+              />
+              {errors.password?.type === "required" && (
+                <span className="text-red-500">This field is required</span>
+              )}
+               {errors.password?.type === "minLength" && (
+        <span className="text-red-500">Minimum length is six</span>
+      )}
+               {errors.password?.type === "maxLength" && (
+        <span className="text-red-500">Maximum length is twenty</span>
+      )}
+               {!errors.password && (
+        <span className="text-green-500">Password met all requirements</span>
+      )}
+            </div>
 
-          <div className="form-control mt-6">
-            <input className="btn btn-primary" type="submit" value="Login" />
-          </div>
+            <div className="form-control mt-6">
+              <input className="btn btn-primary" type="submit" value="Login" />
+            </div>
             <label className="label">
-            <p className="label-text-alt">Already have an account ?
-            <Link to="/login"> Login</Link>
-            </p>
-          </label>
-        </form>
-     
+              <p className="label-text-alt">
+                Already have an account ?<Link to="/login"> Login</Link>
+              </p>
+            </label>
+          </form>
+        </div>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
