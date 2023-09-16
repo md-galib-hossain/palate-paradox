d6cf104e344ef42d581c964a691400edd6b82fa6
md-galib-hossain <mdgalib23@gmail.com>
2023-09-16 16:51:51 +0600

    Make admin added

--- palate-paradox-client/src/Pages/Dashboard/AllUsers/AllUsers.jsx
+++ palate-paradox-client/src/Pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,11 +1,33 @@
 import { useQuery } from "@tanstack/react-query";
 import { Helmet } from "react-helmet-async";
+import { FaTrashAlt, FaUserShield } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const AllUsers = () => {
   const { data: users = [], refetch } = useQuery(["users"], async () => {
     const res = await fetch("http://localhost:5000/users");
     return res.json();
   });
+
+  const handleMakeAdmin = (user) => {
+    fetch(`http://localhost:5000/users/admin/${user._id}`, {
+      method: "PATCH",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount) {
+          refetch();
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `${user.name} is an admin now`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+  };
+  const handleDelete = (user) => {};
   return (
     <div className="w-3/4">
       <Helmet>
@@ -13,30 +35,49 @@ const AllUsers = () => {
       </Helmet>
       <h3 className="text-3xl font-semibold">Total Users:{users.length}</h3>
       <div className="overflow-x-auto">
-  <table className="table table-zebra">
-    {/* head */}
-    <thead>
-      <tr>
-        <th>#</th>
-        <th>Name</th>
-        <th>EMail</th>
-        <th>Role</th>
-        <th>Action</th>
-      </tr>
-    </thead>
-    <tbody>
-      {users.map((user,index)=>  <tr key={user._id}>
-        <th>{index+1}</th>
-        <td>{user.name}</td>
-        <td>{user.email}</td>
-        <td>{user.role}</td>
-        <td>Blue</td>
-      </tr>)}
-     
-     
-    </tbody>
-  </table>
-</div>
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>EMail</th>
+              <th>Role</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr key={user._id}>
+                <th>{index + 1}</th>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>
+                  {user.role === "admin" ? (
+                    "Admin"
+                  ) : (
+                    <button
+                      onClick={() => handleMakeAdmin(user)}
+                      className="btn btn-ghost bg-slate-600 btn-sm text-white"
+                    >
+                      <FaUserShield />
+                    </button>
+                  )}
+                </td>
+
+                <td>
+                  <button
+                    onClick={() => handleDelete(user)}
+                    className="btn btn-ghost bg-red-600 btn-sm text-white"
+                  >
+                    <FaTrashAlt />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
